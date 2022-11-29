196a1d4bcb0d22191ea13801675bcc1aef1b8f92
Benjamin Salmon <bensalmon8@gmail.com>
2022-11-28 18:12:55 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// function to find the lenght of the diagonal of a square of a given side
+
+const S = 9;
+
+function findDiagonal() {
+  return Math.sqrt(2) * S;
+}
+
+console.log(S);
+
+// Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6 and 7.
+
+const side1 = 5;
+const side2 = 6;
+const side3 = 7;
+let side = (side1 + side2 + side3) / 2;
+let area = Math.sqrt(side * ((side - side1) * (side - side2) * (side - side3)));
+console.log(area);
+
+// Write a JavaScript program to find the circumference and surface area of a circle whose radius is 4.
+
+// Write a JavaScript program that accepts two integers and displays the larger of the two.
+
+// Write a JavaScript program that checks whether an integer is an even or an odd number.
